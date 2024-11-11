2ff4fab4b37ba781e87110b7e31bffce5d6241a9
dimqacs <dimqacs@debian.dimqacs>
2024-11-11 13:26:32 +0200

    Added Project Version in pom and updated Enum

--- src/test/java/org/example/enums/FormInputs.java
+++ src/test/java/org/example/enums/FormInputs.java
@@ -1,19 +1,19 @@
 package org.example.enums;
 
 public enum FormInputs {
-    FIRST_NAME("Dima"),
-    LAST_NAME("Dzic"),
-    USER_EMAIL("dzicdmitrii@gmail.com"),
+    FIRST_NAME("John"),
+    LAST_NAME("Deer"),
+    USER_EMAIL("youremail@gmail.com"),
     GENDER("Male"),
-    PHONE_NUMBER("0068303798"),
-    DATE_OF_BIRTH("05 Jun 2005"),
+    PHONE_NUMBER("0068123123"),
+    DATE_OF_BIRTH("01 Jun 2000"),
     SUBJECT_MATH("Maths"),
     SUBJECT_BIOLOGY("Biology"),
     SPORTS_HOBBY_ID("hobbies-checkbox-1"),
     MUSIC_HOBBY_ID("hobbies-checkbox-3"),
     HOBBIES("Sports, Music"),
     FILE_PATH("/home/dimqacs/Java Projects/automatedTesting_I/src/test/resources/pictures/user.png"),
-    ADDRESS("13 Vasile Dokuceaev Street"),
+    ADDRESS("Stefan Cel Mare Street"),
     STATE("NCR"),
     CITY("Delhi");
 
