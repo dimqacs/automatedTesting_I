d6a0c2764532cc69092fe8d4c5a7a23f479d937a
dimqacs <dimqacs@debian.dimqacs>
2024-11-11 16:12:56 +0200

    Fixed the error with SeleniumDriver

--- src/test/java/org/example/utils/SeleniumDriver.java
+++ src/test/java/org/example/utils/SeleniumDriver.java
@@ -1,5 +1,7 @@
 package org.example.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
